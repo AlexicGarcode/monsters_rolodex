6a3fd764cafce9531994f64fa9d0e89b5dca5b21
AlexAutomation <80054118+AlexicGarcode@users.noreply.github.com>
2023-01-27 22:35:56 -0600

    practice setState

--- src/App.js
+++ src/App.js
@@ -3,34 +3,31 @@ import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component() {
-
+class App extends Component {
   constructor(){
     super();
 
-    this.state = { 
-      name:  'Chente'
-    };
+    this.state ={
+      name: 'Ilse',
+    }
   }
 
-
-  render(){
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi this is {this.state.name}
-          </p>
-          <button>Change Name</button>
-        </header>
-      </div>
-    );
-
+render () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hi {this.state.name}</p>
+        <button onClick={() => {
+           this.setState({'name': 'hamburguesa'});
+          }}
+          >
+            Change Name
+            </button>
+      </header>
+    </div>
+  );
   }
-
-  
 }
 
 export default App;
