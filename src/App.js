6bd2dca39b28c27265cc10783d35e06804940468
AlexAutomation <80054118+AlexicGarcode@users.noreply.github.com>
2023-01-28 23:08:03 -0600

    setState lesson 31

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state ={
-      name: 'Ilse',
+      name: {firstName: 'Ilse', lastNAme: 'Flores'},
+      company: 'la Cuchara'
     }
   }
 
@@ -17,9 +18,18 @@ render () {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi {this.state.name}</p>
+        <p>Hi {this.state.name.firstName} {this.state.name.lastName}, 
+        I work at {this.state.company}</p>
         <button onClick={() => {
-           this.setState({'name': 'hamburguesa'});
+           this.setState(()=>{
+              return{
+                name:{firstName: 'Chente', lastName: 'Garcia'}
+              }
+
+           }, ()=>{
+              console.log(this.state);
+           });
+           
           }}
           >
             Change Name
